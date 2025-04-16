70f2f7559126fb83a5f77ae9627e05293d17d935
TonyStark0111 <yedhuku2002@gmail.com>
2025-04-16 10:50:03 +0530

    Update config.py
    😘

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ BOT_STATS_TEXT = os.environ.get("BOTS_STATS_TEXT","<b>BOT UPTIME 🌺</b>\n{upti
 USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "ʙʀᴜʜ ᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ sᴇɴᴘᴀɪ!!")
 
 #your bot token here from https://telegram.me/BotFather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6722606812:AAEdb3pmMLJhK_qHhJsA5eT03bKtgl2SrjQ") 
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6722606812:AAE4EmC8AuizZ5ID3_uVkuLHAEnLPaufBbk") 
 #your api id from https://my.telegram.org/apps
 APP_ID = int(os.environ.get("APP_ID", "15529802"))
 #your api hash from https://my.telegram.org/apps
